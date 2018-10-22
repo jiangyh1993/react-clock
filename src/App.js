d1e628a4823f7c7448a1ad23b50283f864ac5bd9
Unknown <158840284@qq.com>
2018-10-22 00:11:54 -0500

    add a stop button

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Clock from "./Clock";
 
 class App extends Component {
   render() {
+    const books = ["react", "vue", "ng"];
     return (
       <div>
         <Clock />
+        {books.map(book => {
+          return <p key={book}>{book}</p>;
+        })}
       </div>
     );
   }
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -5,11 +5,12 @@ const Clock = props => {
   return (
     <div className="clock">
       <h1>{props.time}</h1>
+      <button onClick={props.stop}>STOP</button>
     </div>
   );
 };
 
-class Clock2 extends React.Component {
+class ClockContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -22,16 +23,21 @@ class Clock2 extends React.Component {
       this.setState({
         time: new Date().toLocaleTimeString()
       });
-    });
+    }, 1000);
   }
 
   componentWillMount() {
     clearInterval(this.timer);
   }
 
+  stop = () => {
+    clearInterval(this.timer);
+  };
+
   render() {
-    return <Clock time={this.state.time} />;
+    console.log(this.props);
+    return <Clock time={this.state.time} stop={this.stop} />;
   }
 }
 
-export default Clock2;
+export default ClockContainer;
